e58fb69121bd986869786255cecb2b429809869e
Elliot Chen <lltchen1@gmail.com>
2019-03-18 17:25:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+   let animal = "cat"
+
   return animal
 }
 
